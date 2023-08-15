3c9b9a4f6ec27d3672c983672f3daa9657120613
fsattar1 <fizzasattar@hotmail.com>
2023-08-15 16:14:31 -0400

    590R day 2 code

---
+++ Day-2-code.R
@@ -0,0 +1,16 @@
+x <- 3
+x^2
+square <- function(x) {
+	value <- x^2
+return(value)
+}
+
+square(x)
+
+raise <- function(x, power){
+		expo <- x^power
+		return(expo)
+}
+raise (x=2, power=3)
+
+use_targets()
